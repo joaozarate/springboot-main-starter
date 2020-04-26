192a173884e4a0e25aafb3c4d14a8d53d8648819
joaozarate <joaozarate@gmail.com>
2020-04-26 07:12:13 +0100

    Project created

---
+++ src/main/java/com/joazarate/mainstarter/SpringbootMainStarterApplication.java
@@ -0,0 +1,13 @@
+package com.joazarate.mainstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMainStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMainStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joazarate/mainstarter/SpringbootMainStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joazarate.mainstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMainStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
